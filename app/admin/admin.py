7c08b9af4c28979cc22481a9cc18d00a97079692
Evgeniy-Developer-droid <evgeniy.python.developer@gmail.com>
2025-05-24 17:05:55 +0300

    admin

--- app/admin/admin.py
+++ app/admin/admin.py
@@ -18,9 +18,10 @@ class AdminAuth(AuthenticationBackend):
     async def login(self, request: Request) -> bool:
         form = await request.form()
         username, password = form["username"], form["password"]
-
+        print(username, password)
         async with async_session_maker() as db_session:
             user = await user_queries.get_user_by_email(username, db_session)
+            print(f"User found: {user}")
             if any([
                 not user,
                 not user.is_active,
@@ -29,6 +30,7 @@ class AdminAuth(AuthenticationBackend):
             ]):
                 return False
             token_hex = uuid4().hex
+            print(f"Token hex: {token_hex}")
             auth_session = await auth_queries.create_auth_session(
                 {
                     "token": token_hex,
@@ -40,34 +42,43 @@ class AdminAuth(AuthenticationBackend):
             )
             token = auth_tools.create_access_token({"sub": token_hex},
                                                    expires_delta=timedelta(minutes=7 * 24 * 60))
+            print(f"Token created: {token}")
             request.session.update({"token": token})
-
+        print(f"Session updated!")
         return True
 
     async def logout(self, request: Request) -> bool:
         # Usually you'd want to just clear the session
+        print(f"Logging out user, clearing session")
         request.session.clear()
         return True
 
     async def authenticate(self, request: Request) -> bool:
         token = request.session.get("token")
-
+        print(f"Authenticating with token: {token}")
         if not token:
             return False
 
         try:
             payload = jwt.decode(token, config.SECRET_KEY, algorithms=[config.ALGORITHM])
+            print(f"Decoded payload: {payload}")
             token_session: str = payload.get("sub")
+            print(f"Token session: {token_session}")
             if token_session is None:
                 return False
             async with async_session_maker() as db_session:
                 auth_session = await auth_queries.get_auth_session(token_session, db_session)
+                print(f"Auth session found: {auth_session}")
                 if not auth_session:
                     return False
                 user = auth_session.user
+                print(f"User found: {user}")
                 if any([not user, not user.is_active, not user.is_superuser]):
+                    print(f"User is not active or not a superuser")
                     return False
+                print(f"User is authenticated: {user.email}")
         except Exception as e:
+            print(f"Exception: {e}")
             print(e)
             return False
 
